bc77f2615bd9cd8745a2619645e3d8f7f2d76472
Javier Mallen <javiermallen@yahoo.es>
2019-01-16 15:13:58 +0100

    Primer commit

---
+++ precache-manifest.cd89d8b9444483fbf4f0f6080966a27c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "caf79dbaca733946ecbe1f0f1fbae1cb",
+    "url": "/img/Ubicacion.caf79dba.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c687ffdc3a0603ba3fe30894e7f83f62",
+    "url": "/img/turismofuentes.c687ffdc.svg"
+  },
+  {
+    "revision": "18989ec3b63952f250ef",
+    "url": "/css/chunk-vendors.1bb56b43.css"
+  },
+  {
+    "revision": "3d5cb522346900e937665d6dc7b72fcf",
+    "url": "/img/FuentesdeEbroescudo.3d5cb522.svg"
+  },
+  {
+    "revision": "29c24dfff0f7c208387c1f7714baaedf",
+    "url": "/img/Foto.29c24dff.svg"
+  },
+  {
+    "revision": "9ab8ccfdcf21ffe3413df8cf2f7bca92",
+    "url": "/img/incidencias.9ab8ccfd.svg"
+  },
+  {
+    "revision": "896805f926ccfeabd165058829dc51ed",
+    "url": "/img/Enviar.896805f9.svg"
+  },
+  {
+    "revision": "3c8e22c170e26092be7a",
+    "url": "/js/app.1b22bc41.js"
+  },
+  {
+    "revision": "18989ec3b63952f250ef",
+    "url": "/js/chunk-vendors.fffcc714.js"
+  },
+  {
+    "revision": "5d49a009988fbecfbc07f22e4da51d9b",
+    "url": "/img/informacion.5d49a009.svg"
+  },
+  {
+    "revision": "87d60de81a98e02f5c948583a8170544",
+    "url": "/img/informacion.87d60de8.jpg"
+  },
+  {
+    "revision": "b7c4cc93666bb09a4a07ca85b816efb2",
+    "url": "/img/noticias.b7c4cc93.svg"
+  },
+  {
+    "revision": "8f6b59be8bfa26bee073c522da2e804e",
+    "url": "/img/Logos.8f6b59be.png"
+  },
+  {
+    "revision": "6b1bcef1ea7a7013138e3d7c0d8537c1",
+    "url": "/img/bando.6b1bcef1.svg"
+  },
+  {
+    "revision": "402e1d60faa9d8d10e3361f2c2094812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c8e22c170e26092be7a",
+    "url": "/css/app.370be7d5.css"
+  }
+];
